e71b4e4ee7df212dfaa2c631091c59814ea971aa
KeatonWakefield001 <keatonwakefield@csus.edu>
2021-11-06 22:06:08 -0700

    i think we are good here

--- src/App.js
+++ src/App.js
@@ -2,25 +2,9 @@ import './App.css';
 
 // run "npm start" in terminal to see webpage
 
-  function App() {
-    return (
+function App() {
+  return (
     <div className="App">
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Testing push
-          Testing D:
-        </a>
-      </header>
       <h1><b>forumerrorly</b></h1>
       <h2><i>Made by the best team in the world B)</i></h2>
     </div>
