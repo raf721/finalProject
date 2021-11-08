02ed67d66e3a8c7c2014f1154b7298d18ee45393
Rafaela Lomboy <rafaela.lomboy@gmail.com>
2021-11-08 14:52:09 -0800

    Added a test thread

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
     <div className="App">
       <h1><b>forumerrorly</b></h1>
       <h2><i>Made by the best team in the world B)</i></h2>
+      <ol>
+        <li class="row">
+          <a href="/item">
+            <h3 class="title">First Thread</h3>
+          </a>
+        </li>
+      </ol>
     </div>
   );
 }
